8f0aeef377165d34e10a02d17a502aa0625736dc
ELIAN <ELIAN@DESKTOP-GV2VSNQ>
2024-05-19 19:13:25 -0500

    fixed bug where the program would jump to next operation after the 2nd
    was done, added while loop

--- CalculadoraAnalicis/src/co/edu/upb/calculadora/Principal.java
+++ CalculadoraAnalicis/src/co/edu/upb/calculadora/Principal.java
@@ -1,103 +1,94 @@
-package co.edu.upb.calculadora;
+    package co.edu.upb.calculadora;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Principal {
+    public class Principal {
 
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Escoja una Operacion (Insertando su Numero): \n"
-				+ "1. Suma\n"
-				+ "2. Resta\n"
-				+ "3. Multiplicacion\n"
-				+ "4. Divizion\n"
-				+ "5. Exponente\n"
-				+ "6. Raiz\n"
-				+ "7. Trigonometricas\n"
-				+ "8. Logaritmo\n"
-				+ "Enter seleccion:   "
-				); 
-		System.out.println(); // crear espacio despuesd de seleccion
-		
-		int seleccion;
-		
-		Scanner receive = new Scanner(System.in);
-		seleccion = receive.nextInt();
-		
-		switch (seleccion) {
-		case 1: {
-			
-			Metodos.sumar();
-		}	
-		case 2: {
-			
-			Metodos.restar();
-			
-		}
-		case 3: {
-			
-			Metodos.multiplicar();
-			
-		}
-		case 4: {
-			
-			Metodos.dividir();
-			
-		}
-		case 5: {
-			
-			Metodos.exponente();
-			
-		}
-		case 6: {
-			
-			Metodos.raizGeneral();
-			
-		}
-		case 7: {
-			System.out.println("Escoja una Operacion (Insertando su Numero): \n"
-					+ "1. sen"
-					+ "2. cos"
-					+ "3. tan");
-			
-			int seleccionTri;
-			
-			Scanner trigonometricas = new Scanner(System.in);
-			seleccionTri = trigonometricas.nextInt();
-			
-			switch (seleccionTri) {
-			case 1: {
-				
-				Metodos.calcularSeno();
-			}	
-			case 2: {
-				
-				Metodos.calcularCoseno();
-				
-			}
-			case 3: {
-				
-				Metodos.calcularTangente();
-				
-			}
-			default:
-				throw new IllegalArgumentException(seleccionTri + "is an invalid seleccion");
-			}
-			
-		}
-		case 8: {
-			
-			Metodos.calcularLogaritmo();
-			
-		}
-		
-		default:
-			throw new IllegalArgumentException(seleccion + "is an invalid seleccion");
-		}
-		
-		
-		
-	}
-	
-}
+        public static void main(String[] args) {
+            Scanner receive = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("Escoja una Operacion (Insertando su Numero): \n"
+                        + "1. Suma\n"
+                        + "2. Resta\n"
+                        + "3. Multiplicacion\n"
+                        + "4. Division\n"
+                        + "5. Exponente\n"
+                        + "6. Raiz\n"
+                        + "7. Trigonometricas\n"
+                        + "8. Logaritmo\n"
+                        + "9. Salir\n"
+                        + "Ingrese su seleccion: ");
+                System.out.println();
+
+                int seleccion = receive.nextInt();
+
+                switch (seleccion) {
+                    case 1: {
+                        Metodos.sumar();
+                        break;
+                    }
+                    case 2: {
+                        Metodos.restar();
+                        break;
+                    }
+                    case 3: {
+                        Metodos.multiplicar();
+                        break;
+                    }
+                    case 4: {
+                        Metodos.dividir();
+                        break;
+                    }
+                    case 5: {
+                        Metodos.exponente();
+                        break;
+                    }
+                    case 6: {
+                        Metodos.raizGeneral();
+                        break;
+                    }
+                    case 7: {
+                        System.out.println("Escoja una Operacion (Insertando su Numero): \n"
+                                + "1. Sen\n"
+                                + "2. Cos\n"
+                                + "3. Tan");
+
+                        int seleccionTri = receive.nextInt();
+
+                        switch (seleccionTri) {
+                            case 1: {
+                                Metodos.calcularSeno();
+                                break;
+                            }
+                            case 2: {
+                                Metodos.calcularCoseno();
+                                break;
+                            }
+                            case 3: {
+                                Metodos.calcularTangente();
+                                break;
+                            }
+                            default:
+                                throw new IllegalArgumentException(seleccionTri + " es una seleccion invalida");
+                        }
+                        break;
+                    }
+                    case 8: {
+                        Metodos.calcularLogaritmo();
+                        break;
+                    }
+                    case 9: {
+                        System.out.println("Saliendo del programa...");
+                        receive.close();
+                        return; 
+                    }
+                    default:
+                        throw new IllegalArgumentException(seleccion + " es una seleccion invalida");
+                }
+
+                System.out.println();
+            }
+        }
+    }
+
